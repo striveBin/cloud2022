6ad3b08ebc68ce6e1823771675700873d5ab03e4
mzb <1376055937@qq.com>
2023-04-16 16:18:10 +0800

    如果获得更完整的信息，使用getForEntity

--- cloud-consumer-order80/src/main/java/com/fc/cloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/fc/cloud/controller/OrderController.java
@@ -4,6 +4,7 @@ import com.fc.cloud.entity.CommonResult;
 import com.fc.cloud.entity.Payment;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,5 +23,15 @@ public class OrderController {
     @GetMapping("/consumer/payment/get/{id}")
     public CommonResult<Payment> getPayment(@PathVariable("id") Long id){
         return restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
+    }
+    @GetMapping("/consumer/payment/getForEntity/{id}")
+    public CommonResult<Payment> getPayment2(@PathVariable("id") Long id){
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+       //状态码2XX是请求成功
+        if (entity.getStatusCode().is2xxSuccessful()){
+            return entity.getBody();
+        }else {
+            return new CommonResult<>(-1,"操作失败");
+        }
     }
 }
