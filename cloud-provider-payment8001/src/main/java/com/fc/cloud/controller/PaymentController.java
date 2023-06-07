2d117f5611849e9b019bbc62c74fdc1d2fff4eec
mzb <1376055937@qq.com>
2023-06-07 21:04:29 +0800

    设置feign的超时时间及日志配置

--- cloud-provider-payment8001/src/main/java/com/fc/cloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/fc/cloud/controller/PaymentController.java
@@ -11,6 +11,7 @@ import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @Slf4j
@@ -60,6 +61,17 @@ public class PaymentController {
     }
     @GetMapping("payment/lb")
     public String getPaymentLB(){
+        return serverPort;
+    }
+
+    @GetMapping("payment/feign/timeout")
+    public String paymentFeignTimeout(){
+        //模拟延迟三秒钟
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
         return serverPort;
     }
 
