a0f82af29df8570c73d547ac79858818f26023ba
mzb <1376055937@qq.com>
2023-07-25 20:27:45 +0800

    hystrix图形监控搭建完成

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/fc/cloud/HystrixDashboardMain9001.java
@@ -0,0 +1,14 @@
+package com.fc.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+//开启图形化
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class,args);
+    }
+}
